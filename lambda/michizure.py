4f9cfef0f704889a01d796d79fa17e2d1aa2740c
Akihiro Harai <jharai0815@gmail.com>
2023-03-01 09:19:39 +0900

    Support new description format #5

--- lambda/michizure.py
+++ lambda/michizure.py
@@ -7,19 +7,19 @@ def lambda_handler(ev, ctx):
   print(ev)
   image_id = ev['detail']['requestParameters']['imageId']
 
-  copyied_ami_desc = (
-      'Copied for DestinationAmi {} from SourceAmi * for SourceSnapshot *. '
-      'Task created on *.').format(image_id)
-
   snapshots = ec2.describe_snapshots(
-      Filters=[{
-          'Name':
-              'description',
-          'Values': [
-              'Created by CreateImage(*) for {} from *'.format(image_id),
-              copyied_ami_desc,
-          ],
-      }])
+      Filters=[
+          {
+              'Name': 'description',
+              'Values': [
+                  'Created by CreateImage(*) for {}'.format(image_id),
+                  'Created by CreateImage(*) for {} from *'.format(image_id),
+                  (
+                      'Copied for DestinationAmi {} from SourceAmi * for '
+                      'SourceSnapshot *. Task created on *.').format(image_id),
+              ],
+          },
+      ])
 
   for snap in snapshots['Snapshots']:
     ec2.delete_snapshot(SnapshotId=snap['SnapshotId'])
